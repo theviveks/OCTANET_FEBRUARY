5b674736da378857d637857c33635ade43a80f94
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-02-17 11:01:24 +0530

    Add files via upload

---
+++ quizapp/js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Who is the current Prime Minister of India?",
+    answer: "Narendra Modi",
+    options: [
+      "Rahul Gandhi",
+      "Narendra Modi",
+      "Manmohan Singh",
+      "Indira Gandhi"
+    ]
+},
+
+  {
+  numb: 2,
+  question: "What is the capital of France?",
+  answer: "Paris",
+  options: [
+    "Rome",
+    "London",
+    "Berlin",
+    "Paris"
+  ]
+},
+  {
+  numb: 3,
+  question: "What is the chemical symbol for water?",
+  answer: "H2O",
+  options: [
+    "CO2",
+    "H2O",
+    "O2",
+    "NaCl"
+  ]
+},
+  {
+  numb: 4,
+  question: "Which planet is known as the Red Planet?",
+  answer: "Mars",
+  options: [
+    "Mars",
+    "Venus",
+    "Jupiter",
+    "Saturn"
+  ]
+},
+  {
+  numb: 5,
+  question: "Who wrote 'Romeo and Juliet'?",
+  answer: "William Shakespeare",
+  options: [
+    "Jane Austen",
+    "William Wordsworth",
+    "Charles Dickens",
+    "William Shakespeare"
+  ]
+},
+];
